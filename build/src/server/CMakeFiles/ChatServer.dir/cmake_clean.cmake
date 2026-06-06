0a694f19a61d8e24399407589ee0010e3ce4268e
Kuwuwu <kehuan19820711@163.com>
2026-06-06 20:01:25 +0800

    使用新版json库

---
+++ build/src/server/CMakeFiles/ChatServer.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "../../../bin/ChatServer"
+  "../../../bin/ChatServer.pdb"
+  "CMakeFiles/ChatServer.dir/chatserver.o"
+  "CMakeFiles/ChatServer.dir/chatserver.o.d"
+  "CMakeFiles/ChatServer.dir/chatservice.o"
+  "CMakeFiles/ChatServer.dir/chatservice.o.d"
+  "CMakeFiles/ChatServer.dir/db/db.o"
+  "CMakeFiles/ChatServer.dir/db/db.o.d"
+  "CMakeFiles/ChatServer.dir/main.o"
+  "CMakeFiles/ChatServer.dir/main.o.d"
+  "CMakeFiles/ChatServer.dir/model/friendmodel.o"
+  "CMakeFiles/ChatServer.dir/model/friendmodel.o.d"
+  "CMakeFiles/ChatServer.dir/model/groupmodel.o"
+  "CMakeFiles/ChatServer.dir/model/groupmodel.o.d"
+  "CMakeFiles/ChatServer.dir/model/offlinemessagemodel.o"
+  "CMakeFiles/ChatServer.dir/model/offlinemessagemodel.o.d"
+  "CMakeFiles/ChatServer.dir/model/usermodel.o"
+  "CMakeFiles/ChatServer.dir/model/usermodel.o.d"
+  "CMakeFiles/ChatServer.dir/redis/redis.o"
+  "CMakeFiles/ChatServer.dir/redis/redis.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
